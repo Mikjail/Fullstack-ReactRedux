804368df35a5c22217159815816723a3dd158f76
Mikjail Salazar <mikjailsalazar@Mikjails-MacBook-Pro.local>
2018-03-12 23:06:10 +0400

    adding new changes

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// key.js
+if(process.env.NODE_ENV === 'productiob'){
+    
+}else{
+   module.export = require('.dev');
+}
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const config = require('./config/keys');
 
-app.get('/', (req, res) =>{
-    res.send({ 'Hi': 'there'});
-})
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(config.mongoURI);
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [config.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
